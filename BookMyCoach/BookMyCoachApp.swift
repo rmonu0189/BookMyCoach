71c7c7937d4c14700e9c8dbfdff0ff8e65bc2d7e
Bharat lal <bharat@digimoplus.com>
2020-11-23 17:08:29 +0530

    Initial commit

---
+++ BookMyCoach/BookMyCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookMyCoachApp.swift
+//  BookMyCoach
+//
+//  Created by Bharat Lal on 11/23/20.
+//
+
+import SwiftUI
+
+@main
+struct BookMyCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookMyCoach/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookMyCoach
+//
+//  Created by Bharat Lal on 11/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
